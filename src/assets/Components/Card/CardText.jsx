25a3d675268ba71c0064933121aa2e4999474df1
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-02-21 08:29:07 +0100

    card text

--- src/assets/Components/Card/CardText.jsx
+++ src/assets/Components/Card/CardText.jsx
@@ -28,3 +28,46 @@ function CardText() {
 
 
 export default CardText;
+
+
+// import { useSelector } from 'react-redux';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
+// function CardText({ ids }) {
+//   const text = useSelector((state) => state.text);
+
+//   console.log('Datos del store:', text);
+
+//  return (
+//   <>
+//     {text && text.filter((t) => ids.includes(t.id)).map((text) => (
+//       <Card key={text.id} style={{ width: '18rem' }}>
+//         <Card.Img variant="top" src={text.image_path} />
+//         <Card.Body>
+//           <Card.Title>{text.title}</Card.Title>
+//           <Card.Text>
+//             {text.description}
+//           </Card.Text>
+//           <Button variant="primary">Go somewhere</Button>
+//         </Card.Body>
+//       </Card>
+//     ))}
+//   </>
+// );
+//     }
+
+
+
+// export default CardText;
+
+//esto se debe poner en las vistas 
+// import CardText from './CardText';
+
+// function View1() {
+//   return <CardText ids={[1, 2, 3]} />;
+// }
+
+// function View2() {
+//   return <CardText ids={[4, 5, 6]} />;
+// }
